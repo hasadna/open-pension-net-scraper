43e87479801453cbee82875f7026cc31f8624386
The Dod <unclezzzen@gmail.com>
2016-11-18 01:52:21 +0200

    Fix pep8

--- web_sources/gemelnet.py
+++ web_sources/gemelnet.py
@@ -7,10 +7,12 @@ import requests
 API_URL = 'http://gemelnet.mof.gov.il/tsuot/ui/tsuotHodXML.aspx'
 OUTPUT_DIR = 'data'
 
+
 def parse_xml(xml):
     return [
         dict([(field.tag, field.text) for field in row])
         for row in xml.iter('Row')]
+
 
 def load_xml_monthly_portfolio(kupa_id, year, month):
     period = '{:04d}{:02d}'.format(year, month)
@@ -23,24 +25,25 @@ def load_xml_monthly_portfolio(kupa_id, year, month):
     req.encoding = 'UTF-8'  # get() sometimes guesses wrong :/
     return XML.fromstring(req.text)
 
+
 def save_csv_monthly_portfolio(kupa_id, year, month):
     xml = load_xml_monthly_portfolio(
         kupa_id, year, month)
     result = parse_xml(xml)
-    filepath = os.path.join(OUTPUT_DIR,
+    filepath = os.path.join(
+        OUTPUT_DIR,
         '{}-{:04d}-{:02d}.csv'.format(kupa_id, year, month))
     sheet = csv.writer(open(filepath, 'w'))
     sheet.writerow(['קופה', 'תקופה', 'קוד', 'נכס', 'כמות'])
     pretty_month = '{:02d}/{:04d}'.format(month, year)
-    
     for r in result:
         sheet.writerow([
-            kupa_id,
-            pretty_month,
+            kupa_id, pretty_month,
             r['ID_NATUN'], r['SHM_NATUN'], r['ERECH_NATUN']])
     return filepath
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("kupa", type=int, help="numeric code of kupa")
