bf3ed68896792c8ff5f8d580a8ebe5a2d8c2c5b9
The Dod <unclezzzen@gmail.com>
2016-12-05 19:56:05 +0200

    Added performance report type (#12)
    See #11

--- batch_gemelnet.py
+++ batch_gemelnet.py
@@ -1,11 +1,12 @@
 import argparse
 from redis import Redis
 from rq import Queue
-from web_sources.gemelnet import save_csv_monthly_portfolio
+from web_sources.gemelnet import (
+    save_csv_monthly_portfolio, save_csv_performance)
 
 
-def get_timeline(kupa, from_year, from_month, to_year, to_month):
-    "Queue (as rq jobs) portfolio CSV dump of kupa for all months in range."
+def get_asset_timeline(kupa, from_year, from_month, to_year, to_month):
+    "Queue (as rq jobs) portfolio CSV dump of kupa for each month in range."
     year = args.from_year
     month = args.from_month
     q = Queue(connection=Redis())
@@ -19,6 +20,17 @@ def get_timeline(kupa, from_year, from_month, to_year, to_month):
     return len(q)
 
 
+def get_performance(kupa, from_year, from_month, to_year, to_month):
+    "Queue (as a single rq job) CSV dump of kupa's performance for month range."
+    q = Queue(connection=Redis())
+    q.enqueue(save_csv_performance, args=(
+        args.kupa, args.from_year, args.from_month,
+        args.to_year, args.to_month))
+    return len(q)
+
+
+OPERATIONS = {'a': get_asset_timeline, 'p': get_performance}
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("kupa", type=int, help="numeric code of kupa")
@@ -26,8 +38,11 @@ if __name__ == '__main__':
     parser.add_argument("from_month", type=int)
     parser.add_argument("to_year", type=int)
     parser.add_argument("to_month", type=int)
+    parser.add_argument(
+        "--type", "-t", choices=['a', 'p'], default='a',
+        help="Report type: assets [default] or performance")
     args = parser.parse_args()
     print("{} jobs in queue".format(
-        get_timeline(
+        OPERATIONS[args.type](
             args.kupa, args.from_year, args.from_month,
             args.to_year, args.to_month)))
--- web_sources/gemelnet.py
+++ web_sources/gemelnet.py
@@ -30,6 +30,21 @@ def _load_xml_monthly_portfolio(kupa_id, year, month):
     return XML.fromstring(req.text)
 
 
+def _load_xml_performance(
+        kupa_id, from_year, from_month, to_year, to_month):
+    "Query Gemelnet for a kupa's performance over a period. Returns XML node."
+    from_date = '{:04d}{:02d}'.format(from_year, from_month)
+    to_date = '{:04d}{:02d}'.format(to_year, to_month)
+    req = requests.get(API_URL, params={
+        'dochot': 1, 'sug': 3,
+        'miTkfDivuach': from_date,
+        'adTkfDivuach': to_date,
+        'kupot': kupa_id
+        })
+    req.encoding = 'UTF-8'  # Gemelnet doesn't declare UTF-8 at header.
+    return XML.fromstring(req.text)
+
+
 def save_csv_monthly_portfolio(kupa_id, year, month):
     """Query Gemelnet for a kupa's monthly portfolio and save as a CSV file.
 Rerurns file path."""
@@ -47,6 +62,30 @@ Rerurns file path."""
         sheet.writerow([
             kupa_id, pretty_month,
             r['ID_NATUN'], r['SHM_NATUN'], r['ERECH_NATUN']])
+    return filepath
+
+
+def save_csv_performance(
+        kupa_id, from_year, from_month, to_year, to_month):
+    """Query Gemelnet for a kupa's performance and save as a CSV file.
+Rerurns file path."""
+    xml = _load_xml_performance(
+        kupa_id, from_year, from_month, to_year, to_month)
+    result = _parse_xml(xml)
+    filepath = os.path.join(
+        OUTPUT_DIR,
+        'perf-{}-{:04d}-{:02d}-{:04d}-{:02d}.csv'.format(
+            kupa_id, from_year, from_month, to_year, to_month))
+    outfile = open(filepath, 'w')
+    outfile.write(str(codecs.BOM_UTF8, 'utf-8'))  # Help Windows detects UTF-8.
+    sheet = csv.writer(outfile)
+    sheet.writerow([
+        'קופה', 'תקופה', 'תשואה נומינלית בפועל', 'יתרת נכסים בפועל'])
+    for r in result:
+        sheet.writerow([
+            kupa_id, '{}/{}'.format(
+                r['TKF_DIVUACH'][4:], r['TKF_DIVUACH'][:4]),
+            r.get('TSUA_NOMINALI_BFOAL', ''), r.get('YIT_NCHASIM_BFOAL', '')])
     return filepath
 
 
